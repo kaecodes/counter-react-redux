42aa08ad9681fc97643a6b85003ff04781fe7bd6
Kaela Nguyen <kaedtn96@gmail.com>
2023-04-05 16:38:46 -0700

    Add 2 more actions to reset and add certain amount

--- src/features/Counter.js
+++ src/features/Counter.js
@@ -1,9 +1,19 @@
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./counterSlice";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
+import { useState } from "react";
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
+
+  const [incrementAmount, setIncrementAmount] = useState(0);
+
+  const addValue = Number(incrementAmount) || 0;
+
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  };
 
   return (
     <div className="counter-container">
@@ -14,6 +24,22 @@ const Counter = () => {
         </button>
         <button className="btn" onClick={() => dispatch(decrement())}>
           -
+        </button>
+      </div>
+      <input
+        type="text"
+        value={incrementAmount}
+        onChange={(e) => setIncrementAmount(e.target.value)}
+      />
+      <div>
+        <button
+          className="btn add"
+          onClick={() => dispatch(incrementByAmount(addValue))}
+        >
+          Add Amount
+        </button>
+        <button className="btn reset" onClick={resetAll}>
+          Reset
         </button>
       </div>
     </div>
